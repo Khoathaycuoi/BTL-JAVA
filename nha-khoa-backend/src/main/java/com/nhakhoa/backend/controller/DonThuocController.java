cb341191d6a4d2f9fd19681ee5289a7c0f0006d3
danghuusy270506-cmd <danghuusy270506@gmail.com>
2026-04-09 02:15:27 +0700

    Update DonThuocController.java

--- nha-khoa-backend/src/main/java/com/nhakhoa/backend/controller/DonThuocController.java
+++ nha-khoa-backend/src/main/java/com/nhakhoa/backend/controller/DonThuocController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/prescriptions")
+@RequestMapping("/api/prescriptions")
 public class DonThuocController {
 
     @Autowired
